e7e5d04a9e72598f1ee8bd7341379c1f2031fcf1
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-05-23 11:45:35 -0400

    update rust downloading script to allow passing config file as a cmdline arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 // src/main.rs
+// TODOS 
+// TODO get plugin name from .git URL.
+// TODO Figure out how to expand ~ to the actual HOME path.
 extern crate toml;
 
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
@@ -9,7 +13,13 @@ use std::process::{Command, Output};
 const PLUGIN_CMDS: &'static str = "/Users/eric/projects/dotfiles/plugins.toml";
 
 fn main() {
-    let mut f = File::open(PLUGIN_CMDS).unwrap();
+    // program small enough that I don't care about panics from .unwrap()
+    let mut f = if let Some(s) = env::args().nth(1) {
+        File::open(&s).unwrap()
+    } else {
+        File::open(PLUGIN_CMDS).unwrap()
+    };
+
     let mut s = String::new();
     f.read_to_string(&mut s);
 
@@ -23,7 +33,6 @@ fn main() {
         let p_tbl = plugin.as_table().unwrap();
         let name = p_tbl.get("name").unwrap().as_str().unwrap();
         let repo = p_tbl.get("repo").unwrap().as_str().unwrap();
-        // TODO get plugin name from .git URL.
         let mut path = PathBuf::new();
         path.push(vundle_dir);
         path.push(name);
